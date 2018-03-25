95573fcb7a38f7f597d20577fd1d4faa22b5546c
Janyelson Victor Lacerda de Oliveira <janyelsonvictor@gmail.com>
2018-03-25 17:18:58 -0300

    Adicionado função de escrever bits no arquivo

--- src/infra/HuffmanWriter.java
+++ src/infra/HuffmanWriter.java
@@ -1,23 +1,76 @@
 package infra;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.HashMap;
 
 public class HuffmanWriter {
 
     public void saveFile(String filePath, HashMap<Integer,Integer> frequency, String finalCode){
 
+        String frequencies = "";
+
+        for (Integer integer : frequency.keySet()){
+            frequencies += integer + " " + frequency.get(integer) + " ";
+        }
+
+        frequencies += "\n";
+
+        try {
+            Files.write(Paths.get(filePath), frequencies.getBytes(), StandardOpenOption.WRITE);
+            writeCode(around(finalCode), filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        /*
+
         try(FileWriter fileWriter = new FileWriter(new File(filePath))){
             for (Integer integer : frequency.keySet()){
                 fileWriter.write(integer + " " + frequency.get(integer) + " ");
             }
-            fileWriter.write("\n" + finalCode);
+            fileWriter.write("\n");
+
+            byte[] b = toBytes(around(finalCode));
+            fileWriter.write(cb);
         } catch (IOException e) {
             System.out.println("Não foi possível salvar o arquivo");
         }
+        */
+    }
 
+    private void writeCode(String finalCode, String filePath) throws IOException
+    {
+        byte[] b = new byte[1];
+
+        int aux = 0, j = 0, k = 7;
+
+        for(int i = 0; i < finalCode.length(); i++)
+        {
+            aux += Character.getNumericValue(finalCode.charAt(i))*Math.pow(2, k);
+            k--;
+
+            if(k < 0)
+            {
+                k = 7;
+                b[0] = (byte) aux;
+                System.out.println("Char: " + (char) aux + " value: " + aux);
+                Files.write(Paths.get(filePath), b, StandardOpenOption.APPEND);
+                aux = 0;
+            }
+        }
+    }
+
+    private String around(String finalCode)
+    {
+        while(finalCode.length()%8 != 0)
+        {
+            finalCode += '0';
+        }
+
+        return finalCode;
     }
 
 }
