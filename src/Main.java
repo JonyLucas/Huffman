0feb9aeca18457dbd2c66fe4b9970f5877bf7e27
Janyelson Victor Lacerda de Oliveira <janyelsonvictor@gmail.com>
2018-03-27 17:13:35 -0300

    Uso de JOptionPane para input

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,23 @@ import controller.HuffmanEncoder;
 import controller.HuffmanCounter;
 import infra.HuffmanWriter;
 
-import javax.swing.JOptionPane;
+import javax.swing.*;
 
 public class Main
 {
     public static void main (String args[])
     {
-        String fileInputPath = "src\\examples\\Exemplo_18K.txt";//JOptionPane.showInputDialog("File Input Path:");
-        String fileOutputPath = "src\\examples\\resultEncoder.txt";
+        String fileInputPath = JOptionPane.showInputDialog("File Input Path:");//"src\\examples\\imagem1.png";
+        String fileOutputPath = "src\\examples\\resultEncoder";
 
         HuffmanCounter huffmanCounter = new HuffmanCounter(fileInputPath);
         huffmanCounter.readAndCount();
 
-        HuffmanEncoder huffmanEncoder = new HuffmanEncoder(fileInputPath, huffmanCounter.getFrenquency());
+        HuffmanEncoder huffmanEncoder = new HuffmanEncoder(fileInputPath, huffmanCounter.getFrequency());
         huffmanEncoder.readAndDecrement();
 
-        new HuffmanWriter().saveFile(fileOutputPath, huffmanCounter.getFrenquency(), huffmanEncoder.getCode());
+        new HuffmanWriter().saveFile(fileOutputPath, huffmanCounter.getFrequency(), huffmanEncoder.getCode());
+
     }
 
 }
