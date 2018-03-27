c4ab154166c300e3bfcb4123ac00cb5bf0fbe626
Janyelson Victor Lacerda de Oliveira <janyelsonvictor@gmail.com>
2018-03-27 17:07:48 -0300

    Trocado value != -1 para value != 300 (fim do arquivo)

--- src/controller/HuffmanCounter.java
+++ src/controller/HuffmanCounter.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 public class HuffmanCounter
 {
     private HuffmanReader huffmanReader;
-    private HashMap <Integer, Integer> frenquency = new HashMap<Integer,Integer>();
+    private HashMap <Integer, Integer> frequency = new HashMap<Integer,Integer>();
     public HuffmanCounter(String filePath)
     {
         huffmanReader = new HuffmanReader(filePath);
@@ -18,19 +18,23 @@ public class HuffmanCounter
         try{
             int value = huffmanReader.nextByte();
 
-            while (value != -1)
+            while (value != 300)
             {
-                int count = (frenquency.get(value) == null) ? 1 : (frenquency.get(value)+1);
-                frenquency.put(value, count);
+                //System.out.println("Byte: " + value);
+                int count = (frequency.get(value) == null) ? 1 : (frequency.get(value)+1);
+                frequency.put(value, count);
                 value = huffmanReader.nextByte();
             }
+
+            System.out.println(frequency);
+
             huffmanReader.close();
         }catch (Exception exception){
             System.out.println(exception.getMessage());
         }
     }
 
-    public void showFrequency(){ System.out.println(frenquency); }
+    public void showFrequency(){ System.out.println(frequency); }
 
-    public HashMap<Integer, Integer> getFrenquency() { return frenquency; }
+    public HashMap<Integer, Integer> getFrequency() { return frequency; }
 }
