45b21bfd33eb99e1d9bc0c910f89e850be20b855
Janyelson Victor Lacerda de Oliveira <janyelsonvictor@gmail.com>
2018-03-27 17:11:56 -0300

    Armazenar os simbolos em Inteiros

--- src/model/HuffmanNode.java
+++ src/model/HuffmanNode.java
@@ -7,7 +7,7 @@ public class HuffmanNode implements Comparable<HuffmanNode>{
     private int frequencySum = 0;
     private boolean isSheet;
     private HuffmanNode leftSon, rightSon;
-    private ArrayList<String> symbols = new ArrayList<String>();
+    private ArrayList<Integer> symbols = new ArrayList<>();
 
 
     public HuffmanNode(HuffmanNode rightSon, HuffmanNode leftSon){
@@ -19,7 +19,7 @@ public class HuffmanNode implements Comparable<HuffmanNode>{
         symbols.addAll(leftSon.getSymbols());
     }
 
-    public HuffmanNode(String symbol, int frequency) {
+    public HuffmanNode(int symbol, int frequency) {
         symbols.add(symbol);
         frequencySum = frequency;
         isSheet = true;
@@ -27,13 +27,13 @@ public class HuffmanNode implements Comparable<HuffmanNode>{
 
     public int getFrequency(){ return this.frequencySum; }
 
-    public ArrayList<String> getSymbols() { return symbols; }
+    public ArrayList<Integer> getSymbols() { return symbols; }
 
     public HuffmanNode getLeftSon() { return leftSon; }
 
     public HuffmanNode getRightSon() { return rightSon; }
 
-    public boolean hasSymbol(String symbol){ return symbols.contains(symbol); }
+    public boolean hasSymbol(int symbol){ return symbols.contains(symbol); }
 
     @Override
     public int compareTo(HuffmanNode node) {
